6aa0841c0ac8d3b325b8486cdf0512eb394cc5bb
iamnavaneetkrish6 <iamnavaneetkrish@gmail.com>
2023-12-03 12:06:02 +0530

    lms Frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { AboutComponent } from './component/about/about.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { UserloginComponent } from './component/userlogin/userlogin.component';
+import { AdminloginComponent } from './component/adminlogin/adminlogin.component';
+import { UserdashboardComponent } from './component/userdashboard/userdashboard.component';
+import { AdmindashboardComponent } from './component/admindashboard/admindashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    UserloginComponent,
+    AdminloginComponent,
+    UserdashboardComponent,
+    AdmindashboardComponent
   ],
   imports: [
     BrowserModule,
